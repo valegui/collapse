e71ace9d413f73e37a859cec650b672f922186a5
Valeria Guidotti <valeriaguidotti@gmail.com>
2025-06-08 14:12:57 -0400

    change spike to area 2d

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,11 +4,16 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var box = $Box
+
+var alive = true
+var box_visible = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
---
+++ scripts/spike.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_detected_body_entered(body: Node2D) -> void:
+	print("damaging player")
+
+
+
+func _on_player_detected_body_exited(body: Node2D) -> void:
+	print("no longer damaging player")

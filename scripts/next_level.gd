22a8e52ff4f8091a61911070d439c4650531ba59
Valeria Guidotti <valeriaguidotti@gmail.com>
2025-06-08 21:51:45 -0400

    expand floor in level 7, level 7 as last level

--- scripts/next_level.gd
+++ scripts/next_level.gd
@@ -5,7 +5,7 @@ func _on_body_entered(body: Node):
 		var scene_path = get_tree().current_scene.scene_file_path
 		var next_scene_n = scene_path.to_int() + 1
 		var next_scene_name: String
-		if next_scene_n == 7:
+		if next_scene_n == 8:
 			next_scene_name = "res://scenes/game_over.tscn"
 		else:
 			next_scene_name = "res://scenes/level_" + str(next_scene_n) + ".tscn"
